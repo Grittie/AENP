1a2f37883898817ed9f1ec8ec3eb4c510c26e364
@gritla <lars.grit@hva.nl>
2023-04-11 20:36:16 +0200

    removed redundant comments

--- src/week5/PetTest.java
+++ src/week5/PetTest.java
@@ -5,18 +5,6 @@ import java.util.Scanner;
 
 public class PetTest {
     public static void main(String[] args) {
-//        // Create multiple Pet instances and add them to an array
-//        Pet[] pets = {
-//                new Dog("Nick", 5, "black"),
-//                new Cat("Ludwig", 3, "yellow"),
-//                new Poodle("Anthony", 12, "white"),
-//                new Tiger("Aiden", 9, "orange")
-//        };
-//
-//        // Loop through the array and call the makeSound() method on each Pet instance
-//        for (Pet pet : pets) {
-//            System.out.println(pet.toString() + " says " + pet.makeSound());
-//        }
 
         System.out.println("Welcome to the Pet Interface.");
         System.out.println("1. Create pet");
